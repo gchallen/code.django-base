f1acd51c1d146e06f1dc8d55b05ce5c8a4755087
Geoffrey Challen <challen@buffalo.edu>
2011-09-06 17:19:36 -0400

    Small whitespace.

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from django.contrib import admin
 admin.autodiscover()
 
