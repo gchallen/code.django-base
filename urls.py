e47fe85aba6ed0958df6c2d51a93a66f33a37ab9
Geoffrey Challen <challen@buffalo.edu>
2011-09-08 10:42:37 -0400

    Cosmetic.

--- urls.py
+++ urls.py
@@ -2,14 +2,19 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
 admin.autodiscover()
 
 # 23 Aug 2011 : GWA : Standard stuff. Admin interface and media.
 
 urlpatterns = patterns('', 
                        url(r'^admin/', include(admin.site.urls)),
-                       (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+                       (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+                      )
 
 # 23 Aug 2011 : GWA : Added for django-course.
 
-urlpatterns += patterns('', (r'^course/', include('course.urls')))
+urlpatterns += patterns('',
+                        (r'^course/', include('course.urls'))
+                       )
